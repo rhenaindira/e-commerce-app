9e1938aa073a6e53dd13706ebde05d9e8117ea17
artinni <madeartini425@gmail.com>
2025-07-30 14:58:12 +0800

    fix: resolve merge conflict in app.js

--- src/app.js
+++ src/app.js
@@ -1,14 +1,21 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+
+const userRoutes = require('./routes/userRoutes');
 const productRoutes = require('./routes/productRoutes');
+
 app.use(express.json()); // Middleware untuk parsing JSON body
+
 // Rute untuk setiap modul
+app.use('/api/users', userRoutes);
 app.use('/api/products', productRoutes);
+
 // Rute dasar
 app.get('/', (req, res) => {
   res.send('Welcome to the E-commerce API!');
 });
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
